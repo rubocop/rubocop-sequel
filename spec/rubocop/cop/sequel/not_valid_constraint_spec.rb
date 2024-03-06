15b28ed844e015eeb2c0d7ad08420b6b4f4e082e
Andrey Subbota <subbota@gmail.com>
2024-03-06 23:53:14 +0100

    Refactor specs

--- spec/rubocop/cop/sequel/not_valid_constraint_spec.rb
+++ spec/rubocop/cop/sequel/not_valid_constraint_spec.rb
@@ -5,30 +5,41 @@ require 'spec_helper'
 describe RuboCop::Cop::Sequel::NotValidConstraint do
   subject(:cop) { described_class.new }
 
-  it 'registers an offense when add_constraint used without valid option' do
-    offenses = inspect_source(<<~RUBY)
-      add_constraint :not_valid, Sequel.lit("jsonb_typeof(column) = 'object'")
-      add_constraint({name: "not_valid"}, Sequel.lit("jsonb_typeof(column) = 'object'"))
-    RUBY
+  context 'when add_constraint used' do
+    it 'registers an offense when used without `not_valid: true` option' do
+      offenses = inspect_source(<<~RUBY)
+        add_constraint :not_valid, Sequel.lit("jsonb_typeof(column) = 'object'")
+        add_constraint({name: "not_valid"}, Sequel.lit("jsonb_typeof(column) = 'object'"))
+      RUBY
 
-    expect(offenses.size).to eq(2)
+      expect(offenses.size).to eq(2)
+    end
+
+    it 'does not register an offense when using `not_valid: true` option' do
+      offenses = inspect_source(<<~RUBY)
+        add_constraint({name: "name", not_valid: true }, Sequel.lit("jsonb_typeof(column) = 'object'"))
+      RUBY
+
+      expect(offenses.size).to eq(0)
+    end
   end
 
-  it 'registers an offense when add_foregn_key used without valid option' do
-    offenses = inspect_source(<<~RUBY)
-      add_foreign_key(:not_valid, :table)
-      add_foreign_key(:not_valid, :table, name: "not_valid")
-    RUBY
+  context 'when add_foreign_key used' do
+    it 'registers an offense when used without `not_valid: true` option' do
+      offenses = inspect_source(<<~RUBY)
+        add_foreign_key(:not_valid, :table)
+        add_foreign_key(:not_valid, :table, name: "not_valid")
+      RUBY
 
-    expect(offenses.size).to eq(2)
-  end
+      expect(offenses.size).to eq(2)
+    end
 
-  it 'does not register an offense when using not valid option' do
-    offenses = inspect_source(<<~RUBY)
-      add_constraint({name: "name", not_valid: true }, Sequel.lit("jsonb_typeof(column) = 'object'"))
-      add_foreign_key(:not_valid, :table, name: "not_valid", not_valid: true)
-    RUBY
+    it 'does not register an offense when using `not_valid: true` option' do
+      offenses = inspect_source(<<~RUBY)
+        add_foreign_key(:not_valid, :table, name: "not_valid", not_valid: true)
+      RUBY
 
-    expect(offenses.size).to eq(0)
+      expect(offenses.size).to eq(0)
+    end
   end
 end
