2b270223831fcb079c30a4b9265c7d84be0f58e8
Andrei Subbota <subbota@gmail.com>
2024-05-06 18:53:57 +0200

    Cleanup specs (#42)

--- spec/rubocop/cop/sequel/migration_name_spec.rb
+++ spec/rubocop/cop/sequel/migration_name_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
-describe RuboCop::Cop::Sequel::MigrationName, :config do
+RSpec.describe RuboCop::Cop::Sequel::MigrationName, :config do
   subject(:cop) { described_class.new(config) }
 
   let(:config) do
--- spec/rubocop/cop/sequel/save_changes_spec.rb
+++ spec/rubocop/cop/sequel/save_changes_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
-describe RuboCop::Cop::Sequel::SaveChanges do
+RSpec.describe RuboCop::Cop::Sequel::SaveChanges do
   subject(:cop) { described_class.new }
 
   it 'registers an offense when using save' do
