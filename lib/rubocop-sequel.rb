bcc0def250aa29484f237073e66bdffc811de3c5
Andrei Subbota <subbota@gmail.com>
2024-05-15 01:22:45 +0200

    Introduce Default Settings and Configurations for RuboCop-Sequel (#41)

--- lib/rubocop-sequel.rb
+++ lib/rubocop-sequel.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require 'rubocop'
-
+require 'rubocop/sequel'
 require 'rubocop/sequel/version'
+require 'rubocop/sequel/inject'
+
+RuboCop::Sequel::Inject.defaults!
+
 require 'rubocop/cop/sequel/concurrent_index'
 require 'rubocop/cop/sequel/json_column'
 require 'rubocop/cop/sequel/migration_name'
