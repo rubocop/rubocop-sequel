880aac6ea185f5d61c9dfc5675d259fb8fe8bf72
Alasdair Wallace Mackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2024-10-25 10:47:49 -0400

    New spec helper: Migration

--- spec/rubocop/cop/sequel/irreversible_migration_spec.rb
+++ spec/rubocop/cop/sequel/irreversible_migration_spec.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Sequel::IrreversibleMigration do
+  include Spec::Helpers::Migration
+
   subject(:cop) { described_class.new }
 
   context 'when inside a change block' do
     let(:invalid_source) do
       <<~SOURCE
-        Sequel.migration do
-          change do
-            alter_table(:stores) do
-              drop_column(:products, :name)
-              drop_index(:products, :price)
-            end
+        change do
+          alter_table(:stores) do
+            drop_column(:products, :name)
+            drop_index(:products, :price)
           end
         end
       SOURCE
@@ -19,43 +19,39 @@ RSpec.describe RuboCop::Cop::Sequel::IrreversibleMigration do
 
     let(:valid_source) do
       <<~SOURCE
-        Sequel.migration do
-          change do
-            alter_table(:stores) do
-              add_primary_key(:id)
-              add_column(:products, :name)
-              add_index(:products, :price)
-            end
+        change do
+          alter_table(:stores) do
+            add_primary_key(:id)
+            add_column(:products, :name)
+            add_index(:products, :price)
           end
         end
       SOURCE
     end
 
     it 'registers an offense when there is an invalid method' do
-      offenses = inspect_source(invalid_source)
+      offenses = inspect_source_within_migration(invalid_source)
       expect(offenses.size).to eq(2)
     end
 
     it 'does not register an offense with valid methods' do
-      offenses = inspect_source(valid_source)
+      offenses = inspect_source_within_migration(valid_source)
       expect(offenses).to be_empty
     end
 
     describe 'and an array is passed into `add_primary_key`' do
       let(:source) do
         <<~SOURCE
-          Sequel.migration do
-            change do
-              alter_table(:stores) do
-                add_primary_key([:owner_id, :name])
-              end
+          change do
+            alter_table(:stores) do
+              add_primary_key([:owner_id, :name])
             end
           end
         SOURCE
       end
 
       it 'registers an offense' do
-        offenses = inspect_source(source)
+        offenses = inspect_source_within_migration(source)
         expect(offenses.size).to eq(1)
       end
     end
@@ -64,20 +60,18 @@ RSpec.describe RuboCop::Cop::Sequel::IrreversibleMigration do
   context 'when inside an up block' do
     let(:source) do
       <<~SOURCE
-        Sequel.migration do
-          up do
-            alter_table(:stores) do
-              add_primary_key([:owner_id, :name])
-              add_column(:products, :name)
-              drop_index(:products, :price)
-            end
+        up do
+          alter_table(:stores) do
+            add_primary_key([:owner_id, :name])
+            add_column(:products, :name)
+            drop_index(:products, :price)
           end
         end
       SOURCE
     end
 
     it 'does not register an offense with any methods' do
-      offenses = inspect_source(source)
+      offenses = inspect_source_within_migration(source)
       expect(offenses).to be_empty
     end
   end
